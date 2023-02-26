4ec5f6a10420944746ef37306df9f0a4e68e6114
Oleksii Bielichenko <byelichenko@gmail.com>
2023-02-27 01:05:47 +0200

    done 3

--- task1.py
+++ task1.py
@@ -6,11 +6,18 @@ user = {
 }
 
 # 1) Find Mike into the user , than if u found Mike create a variable for him
-
+Mike = user["friends"][0]
 # 2) Print Mike at the console
+print("John has friend: ", Mike)
 # 3) Get age of user , and guess when he was born
+Age_of_user = user["age"]
+print("John was born in: ", 2023 - Age_of_user)
 # 4) Skills must look as : Python , QA , Selenium
+
 # 5) Sort john's friends
+
 # 6) After sort find index position of "Bob"
+
 # 7) Add new friends to friends : "Taras" , "Danya" , "Bidden"
+
 # 8) Show how much skills user has
